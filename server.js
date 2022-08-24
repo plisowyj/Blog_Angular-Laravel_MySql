4fdb8af0f5a5de345148835f96a6681c08cbef93
Pablo Lisowyj <plisowyj@gmail.com>
2022-08-23 21:09:22 -0300

    c

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/blog-angular'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/blog-angular/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
